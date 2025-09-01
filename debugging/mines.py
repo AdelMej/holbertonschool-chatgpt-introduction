09727df0ce364b00e523f15e73f8074823f49b77
AdelMej <adelmejrissi@gmail.com>
2025-09-01 11:38:41 +0200

    fix: should now be pyscriptcode compliant

--- debugging/mines.py
+++ debugging/mines.py
@@ -2,8 +2,10 @@
 import random
 import os
 
+
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 class Minesweeper:
     def __init__(self, width=10, height=10, mines=10):
@@ -47,14 +49,22 @@ class Minesweeper:
             for dx in [-1, 0, 1]:
                 for dy in [-1, 0, 1]:
                     nx, ny = x + dx, y + dy
-                    if 0 <= nx < self.width and 0 <= ny < self.height and not self.revealed[ny][nx]:
+                    if (
+                        0 <= nx < self.width
+                        and 0 <= ny < self.height
+                        and not self.revealed[ny][nx]
+                    ):
                         self.reveal(nx, ny)
         return True
 
     def check_win(self):
         for y in range(self.height):
             for x in range(self.width):
-                if (y * self.width + x) not in self.mines and not self.revealed[y][x]:
+                if (
+                    (y * self.width + x)
+                    not in self.mines
+                    and not self.revealed[y][x]
+                ):
                     return False
         return True
 
@@ -74,6 +84,7 @@ class Minesweeper:
                     break
             except ValueError:
                 print("Invalid input. Please enter numbers only.")
+
 
 if __name__ == "__main__":
     game = Minesweeper()
